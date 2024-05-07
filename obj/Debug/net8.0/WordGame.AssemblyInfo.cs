4fadab413942b8fcf33b614214040f6f4b82766a
Kevin Schams <kevinschams04@gmail.com>
2024-05-07 14:43:03 -0500

    Cleaning Git

--- obj/Debug/net8.0/WordGame.AssemblyInfo.cs
+++ obj/Debug/net8.0/WordGame.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WordGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01564b03d72a017e80eca5a852707663fbbb6342")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a847af1c55bc35ef2ff6625fee892d294e4cd841")]
 [assembly: System.Reflection.AssemblyProductAttribute("WordGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WordGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
