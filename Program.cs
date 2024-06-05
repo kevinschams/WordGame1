677c7d5217639074287739819ea534c8d01ddf34
Kevin Schams <schamsk1@students.westerntc.edu>
2024-06-05 13:07:15 -0500

    second yesterday

--- Program.cs
+++ Program.cs
@@ -64,7 +64,7 @@ using WordGame.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlite(connectionString));
@@ -73,7 +73,7 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
-// builder.Services.AddScoped<ApplicationUser>();
+
 
 var app = builder.Build();
 
@@ -85,7 +85,6 @@ if (app.Environment.IsDevelopment())
 else
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -99,8 +98,6 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-// app.MapRazorPages();
-// app.MapRazorPages();
 app.MapFallbackToFile("index.html");
 
 
