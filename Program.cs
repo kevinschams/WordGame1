a29fec524a3f5195d93315c56ff4b8470b0deed1
Kevin Schams <schamsk1@students.westerntc.edu>
2024-06-09 18:24:45 -0500

    elyse's grad day

--- Program.cs
+++ Program.cs
@@ -21,21 +21,6 @@
 
 // builder.Services.AddControllersWithViews();
 
-// var app = builder.Build();
-
-// // Configure the HTTP request pipeline.
-// if (app.Environment.IsDevelopment())
-// {
-//     app.UseMigrationsEndPoint();
-// }
-// else
-// {
-//     app.UseExceptionHandler("/Home/Error");
-//     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-//     app.UseHsts();
-// }
-
-
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using WordGame.Data;
